777f4951409c2d29c27df00c456b5ba7fbbf221f
Victoria <victoria.ad40@gmail.com>
2023-01-14 11:49:12 -0300

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,54 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+    color: #000000;
+    background: #fbf9fb;
+    min-height: 100vh;
+  }
+
+  html,
+  body {
+    max-width: 100vw;
+    overflow-x: hidden;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`
+
+interface ThemeInterface {
+  colors: {
+    primary: string
+  }
+}
+
+const theme: ThemeInterface = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
