b07379c2bb8fa8d8d41008c1ab1f1830a6c14349
Ian Kersz <kerszamaral@gmail.com>
2023-08-22 12:11:23 -0300

    Added photos to all members

--- src/data/membersPage.ts
+++ src/data/membersPage.ts
@@ -8,113 +8,95 @@ interface IMemberPage {
 const orientador: MemberType = {
   name: 'Erika',
   imgUrl:
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
+    '/members/Erika.png',
   course: 'orientador',
   semester: '2005/1',
   interest: 'Teste de Software',
 }
 
 // Os petianes podem ser adicionados em qualquer ordem, pois serão ordenados alfabeticamente
+// imgUrl aceita o link de uma imagem ou o caminho para a imagem dentro da pasta public
 const petianes: MemberType[] = [
   {
     name: 'Galeano',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
+    imgUrl: '/members/Galeano.jpeg',
     course: 'ecp',
     semester: '2021/2',
     interest: 'Desenvolvimento de Software',
   },
   {
     name: 'Helo',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
-
+    imgUrl: '/members/Helo.png',
     course: 'ecp',
     semester: '2021/1',
     interest: 'Sistemas Embarcados e Transmissão de Dados',
   },
   {
     name: 'Ian',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
-
+    imgUrl: '/members/Ian.png',
     course: 'ecp',
     semester: '2021/2',
     interest: 'Otimização de Programas',
   },
   {
     name: 'JV',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
-
+    imgUrl: '/members/JV.jpg',
     course: 'cic',
     semester: '2021/2',
     interest: 'Desenvolvimento de Software',
   },
   {
     name: 'José',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
-
+    imgUrl: '/members/Jose.jpg',
     course: 'cic',
     semester: '2020/2',
     interest: 'Desenvolvimento de Software',
   },
   {
     name: 'Marcos',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
-
+    imgUrl: "/members/Marcos.jpg",
     course: 'cic',
     semester: '2022/2',
     interest: 'Engenharia de Software e IA',
   },
   {
     name: 'Pedro',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
-
+    imgUrl: '/members/Pedro.jpg',
     course: 'ecp',
     semester: '2021/1',
     interest: 'Machine Learning',
   },
   {
     name: 'Sofia',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
-
+    imgUrl: '/members/Sofia.jpeg',
     course: 'cic',
     semester: '2020/1',
     interest: 'Desenvolvimento de Jogos',
   },
   {
     name: 'Tomás',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
+    imgUrl: '/members/Tomas.png',
     course: 'cic',
     semester: '2022/1',
     interest: 'Inteligência Artificial',
   },
   {
     name: 'Laura',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
+    imgUrl: '/members/Laura.jpg',
     course: 'cic',
     semester: '2022/1',
     interest: 'Inteligência Artificial',
   },
   {
     name: 'Beatriz',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
+    imgUrl: '/members/Beatriz.png',
     course: 'cic',
     semester: '2022/1',
     interest: 'Engenharia de Software',
   },
   {
     name: 'Duda',
-    imgUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyLCjkiQJy69HsdW6m_19HttsegclGQOFBQ&usqp=CAU',
+    imgUrl: '/members/Duda.jpeg',
     course: 'cic',
     semester: '2022/1',
     interest: 'Teoria e Desenvolvimento',
